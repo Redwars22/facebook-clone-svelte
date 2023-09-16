481dbc54b28a30a55c8feb2e073966f891be8e37
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-16 11:58:18 -0300

    1 file modified

--- src/lib/profile/ProfileHeader.svelte
+++ src/lib/profile/ProfileHeader.svelte
@@ -9,27 +9,46 @@
 <div class="profile-container">
   <img class="profile-cover" src="{profileCoverURL}">
   <div class="profile-description">
+  <div class="user-picname-container">
     <img class="profile-picture" src="{profileURL}">
-    <div>
+    <div class="profile-info-container">
       <h1>{name}</h1>
-      <span>{numberOfFriends} amigos</span>
-      <span>•</span>
-      <span>{numberOfFollowers} seguindo</span>
+      <div>
+        <span>{numberOfFriends} amigos</span>
+        <span>•</span>
+        <span>{numberOfFollowers} seguindo</span>
+      </div>
     </div>
-    <div>
+    </div>
+    <div class="profile-buttons-container">
       <button class="blue-button-stories">
-        <i class="bi bi-plus"></i>
+        <i class="bi bi-plus-lg"></i>
         Adicionar ao story
       </button>
       <button class="grey-button">
-      <i class="bi bi-pencil"></i>
-      - Editar perfil</button>
-      <button class="grey-button">^</button>
+        <i class="bi bi-pencil"></i>
+        Editar perfil</button>
+      <button class="grey-button">
+        <i class="bi bi-chevron-up"></i>
+      </button>
     </div>
   </div>
 </div>
 
 <style>
+  span {
+    font-size: 15px;
+    font-weight: bold;
+    color: #acb3b8;
+  }
+
+  h1 {
+    font-size: 32px;
+    margin: 0;
+    font-weight: bold;
+    color: #e4e6eb;
+  }
+
   .profile-container {
     width: 940px;
     margin-left: auto;
@@ -41,7 +60,12 @@
     border-radius: 100%;
     border: 6px solid #242526;
     margin-top: -84px;
-    margin-lefT: 2rem;
+    margin-left: 2rem;
+    cursor: pointer;
+  }
+
+  .profile-picture:hover {
+    filter: brightness(115%);
   }
 
   .profile-cover {
@@ -55,6 +79,9 @@
     width: 100%;
     display: flex;
     color: white;
+    justify-content: space-between;
+    align-items: flex-end;
+    border-bottom: 1px solid #3a3b3c;
   }
 
   button {
@@ -64,13 +91,44 @@
     color: white;
     border-radius: 5px;
     border: none;
+    padding: 5px 15px 5px 15px;
+    cursor: pointer;
   }
 
   .blue-button-stories {
     background-color: #2374e1;
   }
 
+  .blue-button-stories:hover {
+    background-color: #4188e8;
+  }
+
   .grey-button {
     background-color: #3a3b3c;
+  }
+
+  .grey-button:hover {
+    background-color: #4d4d4d;
+  }
+
+  .user-picname-container {
+    display: flex;
+  }
+
+  .profile-buttons-container {
+    margin-right: 2rem;
+    height: 100px;
+    display: flex;
+    gap: 8px;
+    align-items: center;
+  }
+
+  .profile-info-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100px;
+    align-items: flex-start;
+    margin-left: 10px;
   }
 </style>
