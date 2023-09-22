4ca3413def4aaf91bdfc074646ae00adfd877b68
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-22 12:18:31 -0300

    1 file modified

--- src/lib/profile/main/PhotosArea.svelte
+++ src/lib/profile/main/PhotosArea.svelte
@@ -1,13 +1,24 @@
 <script>
+  export let friends = [];
 </script>
 
-<div class="photos-area-container">
-  <div><h3>Fotos</h3>
-  <span>Ver todas as fotos</span></div>
+<div class="friends-area-container">
+  <div class="header">
+    <h3>Amigos</h3>
+    <span>Ver todos os amigos</span>
+  </div>
+  <div class="friends-grid">
+    {#each friends as friend}
+    <div class="friend-container">
+      <img src={friend.avatar} alt={friend.name}/>
+      <span>{friend.name}</span>
+    </div>
+    {/each}
+  </div>
 </div>
 
 <style>
-  .photos-area-container {
+  .friends-area-container {
     color: #cac6cb;
     display: flex;
     align-items: flex-start;
@@ -18,14 +29,42 @@
     margin: 15px;
   }
 
-  .photos-area-container > div {
+  .friend-container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    text-align: left;
+    font-size: 13px;
+  }
+
+  .friend-container > span {
+    font-weight: bold;
+    margin-top: 5px;
+  }
+
+  .friends-area-container > .header {
     display: flex;
     justify-content: space-between;
     align-items: center;
     width: 100%;
   }
 
-  span {
+  .friends-area-container > div > span {
     color: #3f84d7;
+  }
+
+  .friends-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 30%);
+    grid-template-rows: repeat(2, auto);
+    gap: 13px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .friend-container > img {
+    border-radius: 8px;
+    width: 101px;
+    height: 101px;
   }
 </style>
