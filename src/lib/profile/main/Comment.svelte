dfc7a13f6bc82f4b50df977959837ab42fd3f27e
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-23 14:09:46 -0300

    1 file modified

--- src/lib/profile/main/Comment.svelte
+++ src/lib/profile/main/Comment.svelte
@@ -1,13 +1,23 @@
 <script>
-  import ProfilePicture from '../../lib/header/ProfilePicture';
+  import ProfilePicture from '../../../lib/header/ProfilePicture.svelte';
 </script>
 
-
-  <ProfilePicture/>
+<div class="comment-area">
+  <ProfilePicture url="https://github.com/Redwars22.png"/>
   <div>
     <input>
     <div>
       <i class="bi bi-person-video3"></i>
       <i class="bi bi-emoji-smile"></i>
+      <i class="bi bi-camera"></i>
+      <i class="bi bi-filetype-gif"></i>
+      <i class="bi bi-sticky"></i>
     </div>
   </div>
+</div>
+
+<style>
+  .comment-area {
+    display: flex;
+  }
+</style>
