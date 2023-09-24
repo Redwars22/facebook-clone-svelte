4c7a3747eea3a0532778bda73f293f125fdbbdc6
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-24 13:25:41 -0300

    1 file modified

--- src/lib/profile/main/Comment.svelte
+++ src/lib/profile/main/Comment.svelte
@@ -5,8 +5,8 @@
 <div class="comment-area">
   <ProfilePicture url="https://github.com/Redwars22.png"/>
   <div class="input-and-buttons-area">
-    <input>
-    <div>
+    <input type="text" placeholder="Comente...">
+    <div class="buttons-area">
       <i class="bi bi-person-video3"></i>
       <i class="bi bi-emoji-smile"></i>
       <i class="bi bi-camera"></i>
@@ -19,10 +19,40 @@
 <style>
   .input-and-buttons-area {
     width: 100%;
+    background-color: #3a3b3c;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+    padding: 8px;
+  }
+
+  input {
+    width: fill-parent;
+    background-color: rgba(0, 0, 0, 0);
+    border: none;
+    color: #b0b3b8;
+  }
+
+  i {
+    color: #9da1a4;
+    padding: 5px;
+    border-radius: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  i:hover {
+    cursor: pointer;
+    background-color: #555656;
   }
 
   .comment-area, .input-and-buttons-area {
     display: flex;
     align-items: center;
+  }
+
+  .buttons-area {
+    display: flex;
   }
 </style>
